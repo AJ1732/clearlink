e629afcc8fbe157edb51bfd840ee49e2a56259bf
AJ1732 <ejemeniboi@gmail.com>
2024-01-23 13:42:04 +0100

    completed sales responsiveness

--- src/components/Sales/RightContent.jsx
+++ src/components/Sales/RightContent.jsx
@@ -4,7 +4,10 @@ import screen_mockup from '../../assets/img/screen_mockup.png'
 const RightContent = () => {
   return (
     <div className=''>
-      <img className='max-w-5xl | absolute  xl:left-[40rem]' src={screen_mockup} />
+      <img 
+        className='max-w-5xl w-5/6 | absolute left-[10vw] |  xl:left-[40rem]' 
+        src={screen_mockup} />
+      <div className='h-[25dvh] md:h-[45dvh] lg:h-[65dvh] xl:hidden'></div>
     </div>
   )
 }
