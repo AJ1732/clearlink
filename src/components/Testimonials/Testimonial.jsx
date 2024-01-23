7b6ea9ab1c3ca5ab8ad832c30ca2c7a86ee93dba
AJ1732 <ejemeniboi@gmail.com>
2024-01-23 12:44:21 +0100

    completed testimonial section responsivesness

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -5,7 +5,7 @@ import Testgrid from './Testgrid'
 const Testimonial = () => {
   return (
     <section className='bg-gray100 | py-24'>
-      <div className='max-w-[88rem] | m-auto px-8 | flex flex-col justify-between items-center | xl:flex-row'>
+      <div className='max-w-[88rem] | m-auto px-8 | flex flex-col justify-between items-center gap-32 | xl:flex-row xl:gap-0'>
         <Content />
         <Testgrid />
       </div>
